241b06242501e3b84c6709d86f959294a06bc7ed
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-02 16:21:49 +0300

    add ✨: extension

---
+++ furniture_store_app_bloc/lib/core/extension/app_extension.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_store_app_bloc/app/model/furniture.dart';
+import 'package:furniture_store_app_bloc/core/animation/animation.dart';
+
+
+extension StringExtension on String {
+  String get addOverFlow {
+    if (length < 15) {
+      return this;
+    } else {
+      return "${substring(0, 15)}...";
+    }
+  }
+}
+
+extension WidgetExtension on Widget {
+  Widget fadeAnimation(double delay) {
+    return FadeInAnimation(delay: delay, child: this);
+  }
+}
+
+extension IntegerExtension on int {
+  get increase => this + 1;
+
+  get decrease => this - 1;
+}
+
+enum Operation { increase, decrease, delete, favorite }
+
+extension ListExtension on List<Furniture> {
+  List<Furniture> operator(Furniture furniture, Operation operation) {
+    switch (operation) {
+      case Operation.increase:
+        furniture = furniture.copyWith(quantity: furniture.quantity.increase);
+        break;
+      case Operation.decrease:
+        furniture = furniture.copyWith(quantity: furniture.quantity.decrease);
+        break;
+      case Operation.delete:
+        furniture = furniture.copyWith(cart: false);
+        break;
+      case Operation.favorite:
+        furniture = furniture.copyWith(isFavorite: !furniture.isFavorite);
+        break;
+    }
+    List<Furniture> list = map((element) {
+      if (element.id == furniture.id) {
+        return furniture;
+      }
+      return element;
+    }).toList();
+
+    return list;
+  }
+}
