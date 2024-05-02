181a8c874c72790b17ecfa2fea841cb9bdc597ec
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-02 16:17:35 +0300

    add ✨: profile screen page

---
+++ furniture_store_app_bloc/lib/app/view/view_profile_screen/view_profile_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:furniture_store_app_bloc/gen/assets.gen.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(Assets.images.profilePic.path),
+          const Text(
+            "Hello Nurhayat!",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(Assets.images.github.path, width: 20),
+              const SizedBox(width: 10),
+              const Text(
+                "github.com/NurhayatYurtaslan",
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
