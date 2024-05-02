0b2a3689bdf5db7aae8a77cc3ebe505730287a1a
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-02 12:25:25 +0300

    add ✨: asset gen files

---
+++ furniture_store_app_bloc/lib/gen/assets.gen.dart
@@ -0,0 +1,303 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/Poppins-Black.ttf
+  String get poppinsBlack => 'assets/fonts/Poppins-Black.ttf';
+
+  /// File path: assets/fonts/Poppins-Italic.ttf
+  String get poppinsItalic => 'assets/fonts/Poppins-Italic.ttf';
+
+  /// File path: assets/fonts/Poppins-Light.ttf
+  String get poppinsLight => 'assets/fonts/Poppins-Light.ttf';
+
+  /// File path: assets/fonts/Poppins-Medium.ttf
+  String get poppinsMedium => 'assets/fonts/Poppins-Medium.ttf';
+
+  /// File path: assets/fonts/Poppins-Regular.ttf
+  String get poppinsRegular => 'assets/fonts/Poppins-Regular.ttf';
+
+  /// File path: assets/fonts/Poppins-SemiBold.ttf
+  String get poppinsSemiBold => 'assets/fonts/Poppins-SemiBold.ttf';
+
+  /// File path: assets/fonts/Poppins-Thin.ttf
+  String get poppinsThin => 'assets/fonts/Poppins-Thin.ttf';
+
+  /// List of all assets
+  List<String> get values => [
+        poppinsBlack,
+        poppinsItalic,
+        poppinsLight,
+        poppinsMedium,
+        poppinsRegular,
+        poppinsSemiBold,
+        poppinsThin
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/comhar_standing_desk_1.jpg
+  AssetGenImage get comharStandingDesk1 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_1.jpg');
+
+  /// File path: assets/images/comhar_standing_desk_2.jpg
+  AssetGenImage get comharStandingDesk2 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_2.jpg');
+
+  /// File path: assets/images/comhar_standing_desk_3.jpg
+  AssetGenImage get comharStandingDesk3 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_3.jpg');
+
+  /// File path: assets/images/comhar_standing_desk_4.jpg
+  AssetGenImage get comharStandingDesk4 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_4.jpg');
+
+  /// File path: assets/images/comhar_standing_desk_5.jpg
+  AssetGenImage get comharStandingDesk5 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_5.jpg');
+
+  /// File path: assets/images/comhar_standing_desk_6.jpg
+  AssetGenImage get comharStandingDesk6 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_6.jpg');
+
+  /// File path: assets/images/comhar_standing_desk_7.jpg
+  AssetGenImage get comharStandingDesk7 =>
+      const AssetGenImage('assets/images/comhar_standing_desk_7.jpg');
+
+  /// File path: assets/images/empty_cart.png
+  AssetGenImage get emptyCart =>
+      const AssetGenImage('assets/images/empty_cart.png');
+
+  /// File path: assets/images/empty_favorite.png
+  AssetGenImage get emptyFavorite =>
+      const AssetGenImage('assets/images/empty_favorite.png');
+
+  /// File path: assets/images/ergonomic_gaming_desk_1.jpg
+  AssetGenImage get ergonomicGamingDesk1 =>
+      const AssetGenImage('assets/images/ergonomic_gaming_desk_1.jpg');
+
+  /// File path: assets/images/ergonomic_gaming_desk_2.jpg
+  AssetGenImage get ergonomicGamingDesk2 =>
+      const AssetGenImage('assets/images/ergonomic_gaming_desk_2.jpg');
+
+  /// File path: assets/images/ergonomic_gaming_desk_3.jpg
+  AssetGenImage get ergonomicGamingDesk3 =>
+      const AssetGenImage('assets/images/ergonomic_gaming_desk_3.jpg');
+
+  /// File path: assets/images/ergonomic_gaming_desk_4.jpg
+  AssetGenImage get ergonomicGamingDesk4 =>
+      const AssetGenImage('assets/images/ergonomic_gaming_desk_4.jpg');
+
+  /// File path: assets/images/ergonomic_gaming_desk_5.jpg
+  AssetGenImage get ergonomicGamingDesk5 =>
+      const AssetGenImage('assets/images/ergonomic_gaming_desk_5.jpg');
+
+  /// File path: assets/images/github.png
+  AssetGenImage get github => const AssetGenImage('assets/images/github.png');
+
+  /// File path: assets/images/intro.jpg
+  AssetGenImage get intro => const AssetGenImage('assets/images/intro.jpg');
+
+  /// File path: assets/images/kana_bamboo_desk_1.jpg
+  AssetGenImage get kanaBambooDesk1 =>
+      const AssetGenImage('assets/images/kana_bamboo_desk_1.jpg');
+
+  /// File path: assets/images/kana_bamboo_desk_2.jpg
+  AssetGenImage get kanaBambooDesk2 =>
+      const AssetGenImage('assets/images/kana_bamboo_desk_2.jpg');
+
+  /// File path: assets/images/kana_bamboo_desk_3.jpg
+  AssetGenImage get kanaBambooDesk3 =>
+      const AssetGenImage('assets/images/kana_bamboo_desk_3.jpg');
+
+  /// File path: assets/images/kana_bamboo_desk_4.jpg
+  AssetGenImage get kanaBambooDesk4 =>
+      const AssetGenImage('assets/images/kana_bamboo_desk_4.jpg');
+
+  /// File path: assets/images/kana_bamboo_desk_5.jpg
+  AssetGenImage get kanaBambooDesk5 =>
+      const AssetGenImage('assets/images/kana_bamboo_desk_5.jpg');
+
+  /// File path: assets/images/kana_bamboo_desk_6.jpg
+  AssetGenImage get kanaBambooDesk6 =>
+      const AssetGenImage('assets/images/kana_bamboo_desk_6.jpg');
+
+  /// File path: assets/images/profile_pic.png
+  AssetGenImage get profilePic =>
+      const AssetGenImage('assets/images/profile_pic.png');
+
+  /// File path: assets/images/soutien_office_chair_1.jpg
+  AssetGenImage get soutienOfficeChair1 =>
+      const AssetGenImage('assets/images/soutien_office_chair_1.jpg');
+
+  /// File path: assets/images/soutien_office_chair_2.jpg
+  AssetGenImage get soutienOfficeChair2 =>
+      const AssetGenImage('assets/images/soutien_office_chair_2.jpg');
+
+  /// File path: assets/images/soutien_office_chair_3.jpg
+  AssetGenImage get soutienOfficeChair3 =>
+      const AssetGenImage('assets/images/soutien_office_chair_3.jpg');
+
+  /// File path: assets/images/soutien_office_chair_4.jpg
+  AssetGenImage get soutienOfficeChair4 =>
+      const AssetGenImage('assets/images/soutien_office_chair_4.jpg');
+
+  /// File path: assets/images/soutien_office_chair_5.jpg
+  AssetGenImage get soutienOfficeChair5 =>
+      const AssetGenImage('assets/images/soutien_office_chair_5.jpg');
+
+  /// File path: assets/images/soutien_office_chair_6.jpg
+  AssetGenImage get soutienOfficeChair6 =>
+      const AssetGenImage('assets/images/soutien_office_chair_6.jpg');
+
+  /// File path: assets/images/theodore_standing_desk_1.jpg
+  AssetGenImage get theodoreStandingDesk1 =>
+      const AssetGenImage('assets/images/theodore_standing_desk_1.jpg');
+
+  /// File path: assets/images/theodore_standing_desk_2.jpg
+  AssetGenImage get theodoreStandingDesk2 =>
+      const AssetGenImage('assets/images/theodore_standing_desk_2.jpg');
+
+  /// File path: assets/images/theodore_standing_desk_3.jpg
+  AssetGenImage get theodoreStandingDesk3 =>
+      const AssetGenImage('assets/images/theodore_standing_desk_3.jpg');
+
+  /// File path: assets/images/theodore_standing_desk_4.jpg
+  AssetGenImage get theodoreStandingDesk4 =>
+      const AssetGenImage('assets/images/theodore_standing_desk_4.jpg');
+
+  /// File path: assets/images/theodore_standing_desk_5.jpg
+  AssetGenImage get theodoreStandingDesk5 =>
+      const AssetGenImage('assets/images/theodore_standing_desk_5.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        comharStandingDesk1,
+        comharStandingDesk2,
+        comharStandingDesk3,
+        comharStandingDesk4,
+        comharStandingDesk5,
+        comharStandingDesk6,
+        comharStandingDesk7,
+        emptyCart,
+        emptyFavorite,
+        ergonomicGamingDesk1,
+        ergonomicGamingDesk2,
+        ergonomicGamingDesk3,
+        ergonomicGamingDesk4,
+        ergonomicGamingDesk5,
+        github,
+        intro,
+        kanaBambooDesk1,
+        kanaBambooDesk2,
+        kanaBambooDesk3,
+        kanaBambooDesk4,
+        kanaBambooDesk5,
+        kanaBambooDesk6,
+        profilePic,
+        soutienOfficeChair1,
+        soutienOfficeChair2,
+        soutienOfficeChair3,
+        soutienOfficeChair4,
+        soutienOfficeChair5,
+        soutienOfficeChair6,
+        theodoreStandingDesk1,
+        theodoreStandingDesk2,
+        theodoreStandingDesk3,
+        theodoreStandingDesk4,
+        theodoreStandingDesk5
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
